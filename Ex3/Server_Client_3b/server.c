8653743a6fee2b10e82f2da2b03644c1fe850e82
Naveen N <115283282+navi004@users.noreply.github.com>
2024-01-23 21:39:29 +0530

    Server

--- Ex3/Server_Client_3b/server.c
+++ Ex3/Server_Client_3b/server.c
@@ -30,8 +30,10 @@ int main()
 	while(1)
 	{
 		bzero(str,100);
-		read(listen_fd,str,100);
+		read(comm_fd,str,100);
 		printf("Echoing back - %s",str);
 		write(comm_fd, str, strlen(str)+1);
 	}
+	close(listen_fd);
+	close(comm_fd);
 }
