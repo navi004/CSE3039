bd592279748b6d9767fd48fb254ebeaeb06f0f7e
Naveen N <115283282+navi004@users.noreply.github.com>
2024-01-24 21:37:29 +0530

    tcp_client.c

--- Ex4/tcp_client.c
+++ Ex4/tcp_client.c
@@ -21,7 +21,7 @@ int main(int argc,char *argv[]) {
         memset(&serv,0,sizeof(serv));
         serv.sin_family = AF_INET;
         serv.sin_port = htons(PORT);
-        serv.sin_addr.s_addr = inet_addr(argv[1]);
+        serv.sin_addr.s_addr = INADDR_ANY;
         
         if(connect(sockfd,(struct sockaddr*) &serv,sizeof(serv)) < 0) {
                 printf("Error in Connect");
