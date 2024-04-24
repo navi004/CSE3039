d5bd58d668eddfbee597afc2314700ef8002a2d8
Naveen N <115283282+navi004@users.noreply.github.com>
2024-04-24 06:03:53 +0530

    Update temp.c

--- Ex7/temp.c
+++ Ex7/temp.c
@@ -1,1 +1,97 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define DELIM "."
+
+/* function to check whether the
+string passed is valid or not */
+bool valid_part(const char* s)
+{
+	int n = strlen(s);
+
+	// if length of passed string is
+	// more than 3 then it is not valid
+	if (n > 3)
+		return false;
+
+	// check if the string only contains digits
+	// if not then return false
+	for (int i = 0; i < n; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return false;
+
+	int x = atoi(s);
+
+	// the string is valid if the number
+	// generated is between 0 to 255
+	return (x >= 0 && x <= 255);
+}
+
+/* return 1 if IP string is
+valid, else return 0 */
+int is_valid_ip(char* ip_str)
+{
+	// if empty string then return false
+	if (ip_str == NULL)
+		return 0;
+	int num, dots = 0;
+	int len = strlen(ip_str);
+	int count = 0;
+
+	// the number dots in the original
+	// string should be 3
+	// for it to be valid
+	for (int i = 0; i < len; i++)
+		if (ip_str[i] == '.')
+			count++;
+	if (count != 3)
+		return false;
+
+	// See following link for strtok()
+
+	char* ptr = strtok(ip_str, DELIM);
+	if (ptr == NULL)
+		return 0;
+
+	while (ptr) {
+
+		/* after parsing string, it must be valid */
+		if (valid_part(ptr)) {
+			/* parse remaining string */
+			ptr = strtok(NULL, ".");
+			if (ptr != NULL)
+				++dots;
+		}
+		else
+			return 0;
+	}
+
+	/* valid IP string must contain 3 dots */
+	// this is for the cases such as 1...1 where
+	// originally the no. of dots is three but
+	// after iteration of the string we find
+	// it is not valid
+	if (dots != 3)
+		return 0;
+	return 1;
+}
+
+// Driver code
+int main()
+{
+	char ip1[] = "128.0.0.1";
+	char ip2[] = "125.16.100.1";
+	char ip3[] = "125.512.100.1";
+	char ip4[] = "125.512.100.abc";
+	is_valid_ip(ip1) ? printf("Valid\n")
+					: printf("Not valid\n");
+	is_valid_ip(ip2) ? printf("Valid\n")
+					: printf("Not valid\n");
+	is_valid_ip(ip3) ? printf("Valid\n")
+					: printf("Not valid\n");
+	is_valid_ip(ip4) ? printf("Valid\n")
+					: printf("Not valid\n");
+	return 0;
+}
 
